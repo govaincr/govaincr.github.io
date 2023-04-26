331895f01ae76a7f5e524a4fd485b4d75a92a51c
NoneForNowButMaybeLater <86891440+NoneForNowButMaybeLater@users.noreply.github.com>
2023-04-26 10:03:10 -0400

    push3

--- home.js
+++ home.js
@@ -1,4 +1,6 @@
 /*----- Comments slider variables start -----*/
+let currentCommentIndex = 0;
+const comments = document.getElementById("comments");
 /*----- Comments slider variables end -----*/
 
 /*----- Comments slider variables start -----*/
